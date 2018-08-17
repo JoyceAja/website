f538780b2f2fb7d6edf0b74abd9848249bc1f6cf
JoyceAja <ajagbe.joyce@gmail.com>
2018-08-17 08:31:02 -0400

    editted my about me paragraph

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -18,10 +18,8 @@ export default class About extends Component {
             like Nanotechnology and Drug Delivery. However, after working on the programming for
             my Capstone Project I realized I particularly enjoyed programming, from the headache and fustration to the accomplished feeling when something finally
             works. So after graduation, I made the decision to pursue software developement. So far my journey has been like a roller coaster 
-            but love for learning continues to propel me to engage in new platform and experiment with new frameworks.
-          </p>
-          <p> A huge career aspiration of mine is 
-            to improve the quality of life of people, I hope to work for an organization whose values aligns with positively impacting people.
+            but my love for learning continues to propel me to engage in new platform and experiment with new frameworks. 
+            Outside of coding, you can catch me on netflix watching criminal shows, chessy romantic comedies and cartoon animations. 
           </p>
         </div>
       </div>
